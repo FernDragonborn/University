4e42c7006806534f4f393ffacfda08ff2760528d
Fern Dragonborn <48766349+FernDragonborn@users.noreply.github.com>
2026-02-14 20:15:23 +0200

    restore cryptography project

--- 7 semestr/БПтД/cryptographyCourse/cryptographyLib/Lab3.cs
+++ 7 semestr/БПтД/cryptographyCourse/cryptographyLib/Lab3.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace cryptographyLib;
+﻿namespace cryptographyLib;
 
 public class FrequencyCracker
 {
--- 7 semestr/БПтД/cryptographyCourse/cryptographyLib/Lab4.cs
+++ 7 semestr/БПтД/cryptographyCourse/cryptographyLib/Lab4.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace cryptographyLib;
+﻿namespace cryptographyLib;
 
 public class LinearCongruentialGenerator(int seed)
 {
